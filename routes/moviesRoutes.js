dad2b39cf7e99192b30527e9c290192cfaffda70
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-06-20 11:34:50 +0200

    validation added

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -1,13 +1,16 @@
 import express from 'express';
+import validate from 'express-validation';
 import { Router } from 'express';
 import moviesController from '../controllers/moviesController';
+import validations from './validation/moviesRoutes';
 
 export default () => {
     const api = Router();
+    api.route('/')
 
-    api.post("/", moviesController.create);
+    .post(validate(validations.create), moviesController.create)
 
-    api.get("/", moviesController.findAll);
+    .get(moviesController.findAll);
 
     return api;
 
---
+++ routes/validation/moviesRoutes.js
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+export default {
+    create: {
+        body: {
+            title: Joi.string().required(),
+        }
+    }
+}
