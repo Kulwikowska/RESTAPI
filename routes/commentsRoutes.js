4688345b19fe3aafa4db33d0d3e57af0927aa5fe
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-06-20 11:11:11 +0200

    movies and comments routes added

---
+++ routes/commentsRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { Router } from 'express';
+import commentsController from '../controllers/commentsController';
+
+export default () => {
+    const api = Router();
+
+    api.post("/", commentsController.create);
+
+    api.get("/", commentsController.findAll);
+
+    return api;
+
+}
---
+++ routes/moviesRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { Router } from 'express';
+import moviesController from '../controllers/moviesController';
+
+export default () => {
+    const api = Router();
+
+    api.post("/", moviesController.create);
+
+    api.get("/", moviesController.findAll);
+
+    return api;
+
+}
