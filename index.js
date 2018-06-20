a86a9f161951d000105366632371b59aa4d761ce
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-06-20 11:13:09 +0200

    change syntax to compare with Babek

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-'use strict';
+import express from 'express';
+import register from 'babel-core/register';
+import commentsRoutes from './routes/commentsRoutes';
+import moviesRoutes from './routes/moviesRoutes';
+import bodyParser from 'body-parser';
+import babelPolyfill from 'babel-polyfill';
+import mongoose from 'mongoose';
 
-const express = require('express');
+const mongoUrl = 'mongodb://localhost:27017/overment';
+mongoose.connect(mongoUrl);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+
 const app = express();
 
-app.listen(8080, '127.0.0.1', function() {
-    console.log('The Server Has Started!');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/movies', moviesRoutes());
+// app.use('/comments', commentsRoutes());
+
+app.listen(1337, () => {
+    console.log('Server running at http://127.0.0.1:1337/');
 });
