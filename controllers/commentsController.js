9c930a4326eaf4ef5316344bf6a9f4de238b38aa
Agnieszka Kulwikowska <agnieszkakulwikowska@MacBook-Air-Agnieszka.local>
2018-06-21 12:23:19 +0200

    filters added

---
+++ controllers/commentsController.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import Comment from '../models/commentsModel';
+import Movie from '../models/moviesModel';
+import bodyParser from 'body-parser';
+import url from 'url';
+import jsonUtils from '../utils/jsonUtils';
+
+export default {
+    async create(req, res) {
+        try {
+            const movie = await Movie.findById(req.body.id).populate('comments');
+            if (movie == null) {
+                throw new Error("Movie doesn't exist.");
+            }
+            const comment = await new Comment({ text: req.body.text, movie: movie._id }).save();
+            movie.Comments.push(comment)
+            movie.save()
+            return res.status(201).send(comment);
+        } catch (error) {
+            return res.status(400).send({ message: error.message });
+        }
+    },
+
+    async findAll(req, res) {
+        let filters = {
+            'movie': req.query.movieId
+        }
+        filters = jsonUtils.removeEmpty(filters);
+        try {
+            const commentsAll = await Comment.find(filters);
+            return res.status(200).send(commentsAll);
+        } catch (error) {
+            return res.status(400).send({ message: error.message });
+        }
+
+    }
+}
